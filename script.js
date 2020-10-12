09f963bd25dd99bd72fdfee03fc9643b66c9900a
Canturilsnaik <italo.cantur@gmail.com>
2020-10-12 20:16:36 -0300

    Feat: Fix bugs and add function for remove tasks

--- script.js
+++ script.js
@@ -30,14 +30,14 @@ function buildBtn(){
   btn.innerHTML = `<img src="./img/download_done.svg" alt="Arrow done">`;
 
   return btn;
-}
+};
 
 function buildEdit(){
   const edit = document.createElement('figure');
   edit.innerHTML = '<img src="img/edit.svg" alt="Edit Habit">';
 
   return edit
-}
+};
 
 function buildParagraph(){
   const p = document.createElement('p');
@@ -47,18 +47,43 @@ function buildParagraph(){
   return p
 };
 
+function buildClose(){
+  const close = document.createElement('button'),
+        length = listGoal.querySelectorAll('li').length;
+
+  // close.innerHTML = '<img src="img/close_big.svg" alt="Close habit">';
+  close.textContent= 'Remove'
+
+  close.setAttribute('data-id', length + 1);
+
+  close.addEventListener('click', (e) => {
+    const element = listGoal.querySelector(`#item-${e.target.getAttribute('data-id')}`);
+    console.log(element)
+    element.remove();
+    e.target.remove();
+  })
+
+  return close;
+}
+
 function createElement() {
   const li = document.createElement('li'),
         inputEdit = document.createElement('input'),
         p = buildParagraph(),
+        close = buildClose(),
         btn = buildBtn(),
         edit = buildEdit(),
         length = listGoal.querySelectorAll('li').length;
 
+  edit.addEventListener('click', () => {
+    p.remove();
+    li.prepend(inputEdit);
+  });
+      
   li.classList.add('list__goals');
   li.id = `item-${length + 1}`;
 
-  li.append(p, btn, edit)
+  li.append(p, btn, edit, close)
   listGoal.append(li);
   
   itemAdd.value = '';
