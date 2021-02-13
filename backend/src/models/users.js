40fde35d5bf4de432be0797f7008186e58e8a73e
Canturilsnaik <italo.cantur@gmail.com>
2021-02-12 23:07:07 -0300

    :sparkles: Feat: Introduce users model

---
+++ backend/src/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const users = mongoose.model('users', userSchema)
+
+module.exports = users;
