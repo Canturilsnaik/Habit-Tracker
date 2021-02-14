6bbe8178237a1630bc717178d60d2fd7847e8949
Canturilsnaik <italo.cantur@gmail.com>
2021-02-14 11:49:02 -0300

    :sparkles: Feat: Method POST for the users

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const user = require('./models/users');
+// const user = require('./controller/users_controller');
+// const express = require('express');
+const users_controller = require('./controllers/users_controller');
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
   return res.json({message: 'It works'})
 });
 
-routes.post('/users', user)
+routes.post('/users', (req, res) => {
+  res.send(users_controller)
+})
 
 module.exports = routes;
