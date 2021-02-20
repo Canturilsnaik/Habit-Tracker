0b9664d615fddb5bb21c5c4ff00a18f5ec01d3ba
Canturilsnaik <italo.cantur@gmail.com>
2021-02-20 00:23:07 -0300

    Feat: Add environment variable with dotenv

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
-// import dotenv from 'dotenv';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
